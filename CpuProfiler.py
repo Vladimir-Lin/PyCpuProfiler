a6cf838effa4421e62c523c937284d49a046ee9f
foxman <lin.vladimir@gmail.com>
2019-10-27 19:11:26 +0800

    add locales

--- CpuProfiler.py
+++ CpuProfiler.py
@@ -26,14 +26,36 @@ from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtGui import QIcon , QCursor
 from PyQt5.QtWidgets import QApplication , QWidget , qApp , QSystemTrayIcon , QMenu , QAction , QActionGroup
 
+Settings     = { }
+Locales      = { }
+Translations = { }
 
+def ActualFile ( filename ) :
+  return os . path . dirname ( os . path . abspath (__file__) ) + "/" + filename
 
+def LoadJSON ( Filename ) :
+  if ( not os . path . isfile ( Filename ) ) :
+    return { }
+  TEXT     = ""
+  with open ( Filename , "rb" ) as cpuFile :
+    TEXT   = cpuFile . read ( )
+  if ( len ( TEXT ) <= 0 ) :
+    return { }
+  BODY = TEXT . decode ( "utf-8" )
+  return json . loads ( BODY )
 
-
-
-def CpuProfilerMain ( ) :
-  print ( "Start" )
-  pass
+def CpuProfilerMain   (                                     ) :
+  global Settings
+  global Locales
+  global Translations
+  Settings     = LoadJSON   ( ActualFile ( "settings.json"        ) )
+  Locales      = LoadJSON   ( ActualFile ( "locales/locales.json" ) )
+  Language     = Settings   [ "Language"                            ]
+  TRFILE       = f"locales/{Language}/translations.json"
+  Translations = LoadJSON   ( ActualFile ( TRFILE                 ) )
+  print ( Settings     )
+  print ( Locales      )
+  print ( Translations )
 
 if __name__ == '__main__':
   CpuProfilerMain ( )
---
+++ locales/locale.py
@@ -0,0 +1,24 @@
+﻿import os
+import sys
+
+pypath = os.path.dirname(os.path.abspath(__file__))
+# pypath = os.path.abspath(os.path.join(pypath,".."))
+
+twass = os.path.abspath(os.path.join(pypath,"zh-TW"))
+cnass = os.path.abspath(os.path.join(pypath,"zh-CN"))
+
+# 切換到簡體中文目錄
+os . chdir ( cnass )
+# 刪除所有檔案
+cmds = "del *.json"
+os.system ( cmds )
+# 切換到選單目錄
+os . chdir ( pypath )
+# 複製繁體中文到簡體中文
+cmds = "copy " + twass + "\\* " + cnass
+os.system ( cmds )
+# 切換到簡體中文目錄
+os . chdir ( cnass )
+os.system ( "TW2CN.exe --suffix json --all" )
+
+print ( "Menu : Convert Traditional Chinese into Simplified Chinese" )
